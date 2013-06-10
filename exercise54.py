20373ce340ee1b1e416a855167e0b8733735ebc8
jplisamaria <jplisamaria@gmail.com>
2013-06-10 19:36:39 -0400

    Exercise 5.4

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,37 @@
 # This is where Exercise 5.4 goes
-# Name:
+# Name: Lisa-Maria Mehta
+
+
+def get_side(side):
+	"""Solicits user input for sidelengths, and returns value as
+	integer.  Exits program if user-entered value is not positive."""
+	print side, '=',
+	length =  raw_input ()
+	if int(length) > 0:
+		return int(length)
+	else:
+		print ('Not positive integer. Program exits.')
+		quit()
+		
+def is_triangle(x,y,z):
+	"""Checks to see if sides x, y, and z, can form a valid
+	triangle."""
+	if (x>=y+z) or (y>=x+z) or (z>=x+y):
+		print "Not a valid triangle."
+	else: 
+		print "Yes, a valid triangle."
+
+
+# ******************
+# ** Main Porgram **
+# ******************
+print 'Please enter sidelengths of triangle as positive integers.'
+
+#	------------------
+#	Getting user input
+x = (get_side ('x'))
+y = (get_side ('y'))
+z = (get_side ('z'))
+#	------------------
+
+is_triangle(x,y,z)
